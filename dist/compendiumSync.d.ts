7e28aa956785d2839bf4d41e6abd6a66e1a56010
mrvauxs <mrvauxs@gmail.com>
2025-02-10 01:04:12 +0100

    fix embeds

--- dist/compendiumSync.d.ts
+++ dist/compendiumSync.d.ts
@@ -1,1 +1,2 @@
+export declare function addHooks(hooks: Record<string, number>): void;
 export default function compendiumSync(): void;
--- src/compendiumSync.ts
+++ src/compendiumSync.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 import type Document from "foundry-pf2e/foundry/common/abstract/document.d.ts";
 
+const { id: moduleID } = __VTT_SYNC_MODULE__;
+
 interface CreateOptions {
 	data: (Record<string, any>)[];
 	modifiedTime: number;
@@ -52,11 +54,126 @@ function documentExportToCLI(rootDoc: Document) {
 	return json;
 }
 
+export function addHooks(hooks: Record<string, number>) {
+	console.groupCollapsed("[foundryvtt-sync] Mounting hooks...");
+	for (const documentType of CONST.COMPENDIUM_DOCUMENT_TYPES) {
+		for (const embed of Object.values(CONFIG[documentType].documentClass.schema.fields)
+			.filter(x => x instanceof foundry.data.fields.EmbeddedCollectionField)
+			.map(x => x.element.documentName as string)
+		) {
+			function getRootDocument(document: Document) {
+				if (!document.pack || !document.pack.startsWith(moduleID)) return;
+
+				let parent: Document | null = document;
+				let highestParent: Document | null = null;
+				while (parent) {
+					parent = parent.parent;
+					if (parent) highestParent = parent;
+				}
+
+				if (highestParent) {
+					console.log(`[foundryvtt-sync] Sending update to parent compendium...\n\t${highestParent.uuid}`);
+					return highestParent;
+				} else {
+					ui.notifications.error(`[foundryvtt-sync] Could not find a parent for document!</p>${document.uuid}`);
+					return false;
+				};
+			}
+
+			hooks[`create${embed}`] = Hooks.on(`create${embed}`, (document) => {
+				const root = getRootDocument(document);
+				if (!root) return;
+				const compendium = game.packs.get(root.pack as string);
+				if (!compendium) return;
+				import.meta.hot?.send(
+					"foundryvtt-compendium-sync:vtt-update",
+					{
+						json: documentExportToCLI(root),
+						dir: compendium.metadata.name,
+					},
+				);
+			});
+			hooks[`update${embed}`] = Hooks.on(`update${embed}`, (document) => {
+				const root = getRootDocument(document);
+				if (!root) return;
+				const compendium = game.packs.get(root.pack as string);
+				if (!compendium) return;
+				import.meta.hot?.send(
+					"foundryvtt-compendium-sync:vtt-update",
+					{
+						json: documentExportToCLI(root),
+						dir: compendium.metadata.name,
+					},
+				);
+			});
+			hooks[`delete${embed}`] = Hooks.on(`delete${embed}`, (document) => {
+				console.log(document);
+			});
+		}
+	}
+
+	hooks.updateCompendium = Hooks.on("updateCompendium", (_collection: CompendiumCollection & { pack: CompendiumCollection }, _data: any, _options: CreateOptions | UpdateOptions | DeleteOptions) => {
+		// Check if _collection isn't the CompendiumFolderCollection instance.
+		// @ts-expect-error No Types Yet
+		if ((_collection instanceof CompendiumFolderCollection)) _collection.metadata = _collection.pack.metadata;
+
+		if (_collection?.metadata?.packageName !== moduleID) return;
+
+		const dir = _collection.metadata.name;
+
+		if ("data" in _options) {
+			_options.data.forEach((doc) => {
+				const document = _collection.get(doc._id);
+				console.log("A Document has been created.", document);
+
+				if (!document) throw new Error("Document not found!?");
+
+				import.meta.hot?.send(
+					"foundryvtt-compendium-sync:vtt-update",
+					{
+						json: documentExportToCLI(document),
+						dir,
+					},
+				);
+			});
+		}
+
+		if ("updates" in _options) {
+			_options.updates.forEach((update) => {
+				const document = _collection.get(update._id);
+				console.log("A Document has been updated.", document);
+
+				if (!document) throw new Error("Document not found!?");
+
+				import.meta.hot?.send(
+					"foundryvtt-compendium-sync:vtt-update",
+					{
+						json: documentExportToCLI(document),
+						dir,
+					},
+				);
+			});
+		}
+
+		if ("deleteAll" in _options) {
+			_options.ids.forEach((id) => {
+				console.log("A Document has been deleted.", id);
+
+				import.meta.hot?.send(
+					"foundryvtt-compendium-sync:vtt-delete",
+					{ id, dir },
+				);
+			});
+		}
+	});
+	console.groupEnd();
+};
+
 export default function compendiumSync() {
 	if (import.meta.hot) {
 		const { id: moduleID } = __VTT_SYNC_MODULE__;
 
-		const hooks = {
+		const hooks: Record<string, number> = {
 			ready: Hooks.on("ready", () => {
 				// #region Libwrapper
 				// https://github.com/foundryvtt/foundryvtt/issues/12134
@@ -101,74 +218,30 @@ export default function compendiumSync() {
 						default: true,
 					},
 				});
-			}),
-			updateCompendium: Hooks.on("updateCompendium", (_collection: CompendiumCollection & { pack: CompendiumCollection }, _data: any, _options: CreateOptions | UpdateOptions | DeleteOptions) => {
-			// Check if _collection isn't the CompendiumFolderCollection instance.
-			// @ts-expect-error No Types Yet
-				if ((_collection instanceof CompendiumFolderCollection)) _collection.metadata = _collection.pack.metadata;
 
-				if (_collection?.metadata?.packageName !== moduleID) return;
-
-				const dir = _collection.metadata.name;
-
-				if ("data" in _options) {
-					_options.data.forEach((doc) => {
-						const document = _collection.get(doc._id);
-						console.log("A Document has been created.", document);
-
-						if (!document) throw new Error("Document not found!?");
-
-						import.meta.hot?.send(
-							"foundryvtt-compendium-sync:vtt-update",
-							{
-								json: documentExportToCLI(document),
-								dir,
-							},
-						);
-					});
-				}
-
-				if ("updates" in _options) {
-					_options.updates.forEach((update) => {
-						const document = _collection.get(update._id);
-						console.log("A Document has been updated.", document);
-
-						if (!document) throw new Error("Document not found!?");
-
-						import.meta.hot?.send(
-							"foundryvtt-compendium-sync:vtt-update",
-							{
-								json: documentExportToCLI(document),
-								dir,
-							},
-						);
-					});
-				}
-
-				if ("deleteAll" in _options) {
-					_options.ids.forEach((id) => {
-						console.log("A Document has been deleted.", id);
-
-						import.meta.hot?.send(
-							"foundryvtt-compendium-sync:vtt-delete",
-							{ id, dir },
-						);
-					});
-				}
+				addHooks(hooks);
 			}),
 		};
 
 		// HMR
-		import.meta.hot.accept();
-		import.meta.hot.dispose(() => Object.entries(hooks).forEach(([k, v]) => Hooks.off(k, v)));
+		import.meta.hot?.dispose(() => {
+			console.groupCollapsed(`[foundryvtt-sync] Unmounting ${Object.keys(hooks).length} hooks...`);
+			Object.entries(hooks).forEach(([k, v]) => Hooks.off(k, v));
+			console.groupEnd();
+		});
+		import.meta.hot?.accept((newModule) => {
+			if (newModule) {
+				newModule.addHooks(hooks);
+			}
+		});
 
 		// Messages
-		import.meta.hot.on(
+		import.meta.hot?.on(
 			"foundryvtt-compendium-sync:vtt-update:response",
 			({ data: { json } }) => console.log(`Received and saved ${json.name} (${json._id})`),
 		);
 
-		import.meta.hot.on("foundryvtt-compendium-sync:system-update", async ({ json, timestamp, file }) => {
+		import.meta.hot?.on("foundryvtt-compendium-sync:system-update", async ({ json, timestamp, file }) => {
 			const data = JSON.parse(json);
 			let document: Document | null | undefined;
 
